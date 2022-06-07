cf3d5574bb14f7c7d6964e4d95e424c22a633a33
Martin Lillepuu <martin@mlp.ee>
2022-06-07 12:04:39 +0300

    Revert "Merge remote-tracking branch 'upstream/commatwo_master' into subaru-community-neos20"
    This reverts commit 8e47cab9f047c54956277f3b89f085cf913a7fda, reversing
    changes made to 3650afd9979cb2a5e2c972a982710d39cb63f6d2.

--- selfdrive/ui/qt/util.cc
+++ selfdrive/ui/qt/util.cc
@@ -18,7 +18,7 @@ QString getBrand() {
 }
 
 QString getBrandVersion() {
-  return getBrand() + " v" + getVersion().left(16).trimmed();
+  return getBrand() + " v" + getVersion().left(14).trimmed();
 }
 
 QString getUserAgent() {

1dbdefcbf15a4f5e99e7ad355afbd364402f3d48
Martin Lillepuu <martin@mlp.ee>
2022-11-12 12:13:55 +0000

    Add false

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -85,12 +85,14 @@ TogglesPanel::TogglesPanel(SettingsWindow *parent) : ListWidget(parent) {
       tr("Subaru Manual PB Stop and Go"),
       tr("Experimental feature to enable stop and go for Subaru Global models with manual handbrake. Models with electric parking brake should keep this disabled."),
       "../assets/offroad/icon_speed_limit.png",
+      false,
     },
     {
       "FirmwareQueryDelay",
       tr("Firmware query delay"),
       tr("Add 10 second delay before running FPv2 ECU firmware queries. May be needed for Subaru pre-global models"),
       "../assets/offroad/icon_speed_limit.png",
+      false,
     },
 #ifdef ENABLE_MAPS
     {
